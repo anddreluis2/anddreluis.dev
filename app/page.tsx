40bd7aeca12db3b39cfbe15ca37a281658593342
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2024-03-30 21:04:09 -0300

    add social

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>andre luis de oliveira</div>
+    <main className="flex min-h-screen flex-col items-center justify-between py-72">
+      <div className="text-lg uppercase flex shadow-lg shadow-stone-900 justify-center items-center">
+        andré luis de oliveira andrade
+      </div>
+      <div className="flex justify-between w-72">
+        <a className="cursor-pointer hover:underline">github</a>
+        <a className="cursor-pointer hover:underline">twitter</a>
+        <a className="cursor-pointer hover:underline">linkedin</a>
+      </div>
     </main>
   );
 }
