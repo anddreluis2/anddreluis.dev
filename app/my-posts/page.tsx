10b3451d85d2220ee122acafaac7516a4168081f
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2024-04-06 17:18:16 -0300

    add title

--- app/my-posts/page.tsx
+++ app/my-posts/page.tsx
@@ -11,7 +11,8 @@ export default async function MyPosts() {
   const posts = await getPosts();
 
   return (
-    <main className="flex min-h-screen flex-col items-start font-xs px-8 py-10 sm:py-16 sm:px-48">
+    <main className="flex min-h-screen flex-col items-start font-xs px-10 py-10 sm:py-16 sm:px-48">
+      <h1 className="text-3xl font-extrabold mb-6">My articles</h1>
       {posts.map((post: PostsType) => (
         <Link
           target="blank"
