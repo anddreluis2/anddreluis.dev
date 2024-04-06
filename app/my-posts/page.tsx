3fc7c81e88a6250d7ff08fb9f4206923981b4407
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2024-04-06 17:32:06 -0300

    add publish day

--- app/my-posts/page.tsx
+++ app/my-posts/page.tsx
@@ -5,6 +5,7 @@ type PostsType = {
   title: string;
   url: string;
   id: string;
+  published_timestamp: string;
 };
 
 export default async function MyPosts() {
@@ -14,14 +15,18 @@ export default async function MyPosts() {
     <main className="flex min-h-screen flex-col items-start font-xs px-10 py-10 sm:py-16 sm:px-48">
       <h1 className="text-3xl font-extrabold mb-6">My articles</h1>
       {posts.map((post: PostsType) => (
-        <Link
-          target="blank"
-          href={`${post.url}`}
-          className="hover:cursor-pointer underline pb-2"
-          key={post.id}
-        >
-          {post.title}
-        </Link>
+        <div className="flex pb-10 sm:pb-0 sm:gap-6" key={post.id}>
+          <p className="w-full sm:w-auto">
+            {post.published_timestamp.slice(0, 10)}
+          </p>
+          <Link
+            target="blank"
+            href={`${post.url}`}
+            className="hover:cursor-pointer sm:w-auto w-full underline pb-2"
+          >
+            {post.title}
+          </Link>
+        </div>
       ))}
     </main>
   );
